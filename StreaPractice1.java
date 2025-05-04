c04b2eaae6800f08b908104cdd2a1888ecafc4db
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2025-05-04 23:57:38 +0530

    Stream implemented 2

--- StreaPractice1.java
+++ StreaPractice1.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreaPractice1 {
     public static void main(String[] args) {
@@ -13,18 +15,64 @@ public class StreaPractice1 {
         final List<String> nameList = Arrays.asList("Alice", "Bob", "Anna", "Gary");
         final List<String> alphanumericList = Arrays.asList("a124", "1dsa78", "weq78985assd");
 
+        System.out.println();
+        System.out.println();
+
         //1. Given a sentence find the word that has the highest length
         String longestString = Arrays
             .stream(commonString.split(" "))
             .max(Comparator.comparing(s -> s.length()))
             .get();
         //2. Remove duplicates from the string and return in the same order
+        List<String> distinctWords = Arrays
+            .stream(commonString.split(" "))
+            .distinct()
+            .collect(Collectors.toList());
+
         //3. Find the word that has the second highest length
+        String topTwoStrings = Arrays
+            .stream(commonString.split(" "))
+            .sorted(Comparator.comparing((String s) -> s.length()).reversed())
+            .limit(2)
+            .skip(1)
+            .findFirst()
+            .get();
+        System.out.println("topTwoStrings: " + topTwoStrings);
+
         //4. Find the 2nd highest word length in the given sentence
+        Integer secondLongestStringLength = Arrays
+            .stream(commonString.split(" "))
+            .map(s -> s.length())
+            .sorted(Comparator.reverseOrder())
+            .skip(1)
+            .findFirst()
+            .get();
+
         //5. Given a sentence, find the occurrence of each word
+        Map<String, Long> stringCountMap = Arrays
+            .stream(commonString.split(" "))
+            .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+
         //6. Given a sentence, find the words with a specified number of vowels
+        List<String> filteredStrings = Arrays
+            .stream(commonString.split(" "))
+            .filter((String s) -> s.replaceAll("[^aeiouAEIOU]", "").length() == 2)
+            .collect(Collectors.toList());
+
         //7. Divide given integer list into lists of even and odd numbers
+        List<List<Integer>> sortedNumberGroups = Arrays
+            .stream(commonNumArr)
+            .boxed()
+            .collect(Collectors.groupingBy(x -> x % 2, Collectors.toList()))
+            .values()
+            .stream()
+            .collect(Collectors.toList());
+
         //8. Given a word, find the occurrence of each character
+        Map<String, Long> characterFrequencyMap = Arrays
+            .stream(commonString.split(""))
+            .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+
         //9. Arrange the numbers in Descending/Ascending Order
         //10. Given an array, find the sum of unique elements
         //11. Given a string, find the first non-repeated character
