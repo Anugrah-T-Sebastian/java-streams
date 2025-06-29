fc07759d34d9c5287d8adee53a8fc45a4e7ad428
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2025-06-29 23:59:41 +0530

    Update StreaPractice1.java
    Updates

--- StreaPractice1.java
+++ StreaPractice1.java
@@ -63,314 +63,134 @@ public class StreaPractice1 {
         System.out.println();
 
         //1. Given a sentence find the word that has the highest length
-        String longestString = Arrays
-            .stream(commonString.split(" "))
-            .max(Comparator.comparing(s -> s.length()))
-            .get();
+        String maxLength = Arrays
+                .stream(commonString.split(" "))
+                .max(Comparator.comparing(s -> s.length()))
+                .get();
+
         //2. Remove duplicates from the string and return in the same order
-        List<String> distinctWords = Arrays
-            .stream(commonString.split(" "))
-            .distinct()
-            .collect(Collectors.toList());
+        String distinctCharString = Arrays
+                .stream(commonString.split(""))
+                .distinct()
+                .collect(Collectors.joining());
 
         //3. Find the word that has the second highest length
-        String topTwoStrings = Arrays
-            .stream(commonString.split(" "))
-            .sorted(Comparator.comparing((String s) -> s.length()).reversed())
-            .limit(2)
-            .skip(1)
-            .findFirst()
-            .get();
-        System.out.println("topTwoStrings: " + topTwoStrings);
+        String secondLargestWord = Arrays
+                .stream(commonString.split(" "))
+                .sorted(Comparator.comparing((String s) -> s.length()).reversed())
+                .skip(1)
+                .findFirst()
+                .get();
 
         //4. Find the 2nd highest word length in the given sentence
-        Integer secondLongestStringLength = Arrays
-            .stream(commonString.split(" "))
-            .map(s -> s.length())
-            .sorted(Comparator.reverseOrder())
-            .skip(1)
-            .findFirst()
-            .get();
+        Integer secondHighestWordLength = Arrays
+                .stream(commonString.split(" "))
+                .map(x -> x.length())
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .get();
 
         //5. Given a sentence, find the occurrence of each word
-        Map<String, Long> stringCountMap = Arrays
-            .stream(commonString.split(" "))
-            .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+        Map<String, Long> wordCountMap = Arrays
+                .stream(commonString.split(" "))
+                .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
 
         //6. Given a sentence, find the words with a specified number of vowels
-        List<String> filteredStrings = Arrays
-            .stream(commonString.split(" "))
-            .filter((String s) -> s.replaceAll("[^aeiouAEIOU]", "").length() == 2)
-            .collect(Collectors.toList());
+        List<String> wordsWithVowels = Arrays
+                .stream(commonString.split(" "))
+                .filter((String s) -> s.replaceAll("[^aeiouAEIOU]", "").length() == 2)
+                .toList();
 
         //7. Divide given integer list into lists of even and odd numbers
-        List<List<Integer>> sortedNumberGroups = Arrays
-            .stream(commonNumArr)
-            .boxed()
-            .collect(Collectors.groupingBy(x -> x % 2, Collectors.toList()))
-            .values()
-            .stream()
-            .collect(Collectors.toList());
+        List<List<Integer>> oddEvenList = IntStream
+                .range(1, 10)
+                .boxed()
+                .collect(Collectors.groupingBy(x -> x % 2 == 0, Collectors.toList()))
+                .entrySet()
+                .stream()
+                .map(x -> x.getValue())
+                .toList();
 
         //8. Given a word, find the occurrence of each character
-        Map<String, Long> characterFrequencyMap = Arrays
-            .stream(commonString.split(""))
-            .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+        Map<String, Long> characterOccurence = Arrays
+                .stream(commonString.split(""))
+                .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
 
         //9. Arrange the numbers in Descending/Ascending Order
-        List<Integer> reversedNumberList = Arrays
-            .stream(commonNumArr)
-            .boxed()
-            .sorted(Comparator.reverseOrder())
-            .collect(Collectors.toList());
+        List<Integer> sortedArray = Arrays
+                .stream(commonNumArr)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .toList();
 
         //10. Given an array, find the sum of unique elements
-        Integer sumOfDistinctNumbers = Arrays
-            .stream(commonNumArr)
-            .boxed()
-            .distinct()
-            .reduce(0, (a, b) -> a + b);
-
+        Integer sumOfUniqueNumber = Arrays
+                .stream(commonNumArr)
+                .boxed()
+                .distinct()
+                .reduce(0, (a, b) -> a + b);
+        
         //11. Given a string, find the first non-repeated character
-        String firstUniqueChar = Arrays
-            .stream(commonString.split(""))
-            .filter(s -> commonString.indexOf(s) == commonString.lastIndexOf(s))
-            .findFirst()
-            .get();
+        String firstNonRepeatedCharacter = Arrays
+                .stream(commonString.split(""))
+                .filter(x -> commonString.indexOf(x) == commonString.lastIndexOf(x))
+                .findFirst()
+                .get();
 
         //12. Given a string, find the first repeated character
-        String firstDuplicateChar = Arrays
-            .stream(commonString.split(""))
-            .filter(s -> commonString.indexOf(s) != commonString.lastIndexOf(s))
-            .findFirst()
-            .get();
+        String firstRepeatCharacter = Arrays
+                .stream(commonString.split(""))
+                .filter(x -> commonString.indexOf(x) < commonString.lastIndexOf(x))
+                .findFirst()
+                .get();
 
         //13. Given an array of integers, group the numbers by the range
-        Map<Integer, List<Integer>> groupedByHundreds = Arrays
-            .stream(commonNumArr)
-            .mapToObj(x -> x)
-            .collect(Collectors.groupingBy(x -> x / 100, () -> new TreeMap<>(), Collectors.toList()));
+        TreeMap<Integer, List<Integer>> groupRangedElements = Arrays
+                .stream(commonNumArr)
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        x -> x / 10,
+                        () -> new TreeMap<Integer, List<Integer>>(),
+                        Collectors.toList()));
 
         //14. Given a list of strings, create a list that contains only integers
-        List<Integer> filteredIntegersList = Arrays
-            .stream(mixedList)
-            .filter(x -> {
+        List<Integer> intergerList = Arrays
+                .stream(mixedList)
+                .filter(x -> {
                     try {
                         Integer.parseInt(x);
                         return true;
-                    } catch (Exception e) {
+                    } catch (RuntimeException e) {
                         return false;
                     }
-            })
-            .map(Integer::parseInt)
-            .collect(Collectors.toList());
+                })
+                .map(Integer::parseInt)
+                .toList();
+
+        System.out.println("Integer list: " + intergerList);
 
         //15. Find the products of the first two elements in an array
-        Integer limitedProduct = Arrays
-            .stream(commonNumArr)
-            .limit(2)
-            .boxed()
-            .reduce(1, (a, b) -> a * b);
-        
+        Integer productOfTwoElements = Arrays
+                .stream(commonNumArr)
+                .boxed()
+                .limit(2)
+                .reduce(1, (a, b) -> a * b);
+
         //16. Group /Pair anagrams from a list of Strings
-        List<List<String>> anagramGroups = Arrays
-            .stream(commonString.split(" "))
-            .collect(Collectors.groupingBy(s -> {
-                char[] charArr = s.toCharArray();
-                Arrays.sort(charArr);
-                return charArr.toString();
-            }))
-            .values()
-            .stream()
-            .collect(Collectors.toList());
-
-        System.out.println("anagramGroups: " + anagramGroups);
-
-        //17. Write a stream program to multiply alternative numbers in an array
-        int productOfEvenIndexedElements = IntStream
-            .range(0, commonNumArr.length)
-            .filter(x -> x % 2 == 0)
-            .map(x -> commonNumArr[x])
-            .reduce(1, (a, b) -> a * b);
-
-        //18. Write a program to multiply 1st and last element, 2nd and 2nd last element
-        List<Integer> multipliedResults = IntStream
-            .range(0, commonNumArr.length)
-            .map(x -> commonNumArr[x] * commonNumArr[commonNumArr.length - x - 1])
-            .boxed()
-            .collect(Collectors.toList());
-
-        //19. Write a stream program to move all zero’s to beginning of array
-        List<Integer> zerosMovedToLeft = Arrays
-            .stream(commonNumArr)
-            .boxed()
-            .collect(Collectors.groupingBy(x -> x != 0))
-            .values()
-            .stream()
-            .flatMap(x -> x.stream())
-            .collect(Collectors.toList());
-
-        System.out.println("zerosMovedToLeft: " + zerosMovedToLeft);
-
-        //20. In a given array of integers, return true if it contains distinct values
-        Boolean distinctCheck = (numberList1
-            .stream()
-            .distinct()
-            .count() == numberList1.size());
-
-        //21. Given the string[] group the strings based on the middle character
-        List<List<String>> groupedStringsByMidChar = Arrays
-            .stream(commonString.split(" "))
-            .collect(Collectors.groupingBy(x -> x.charAt(x.length() / 2)))
-            .values()
-            .stream()
-            .collect(Collectors.toList());
-            
-        //22. Find the sum of all the elements in a list
-        int totalSum = numberList1
-            .stream()
-            .mapToInt(x -> x)
-            .sum();
-
-        //23. Sort a list of strings in alphabetical order
-        List<String> sortedNameList = nameList
-            .stream()
-            .sorted()
-            .collect(Collectors.toList());
-
-        //24. Convert a list of integers to a list of their squares
-        List<Integer> squaredNumbers = numberList1
-            .stream()
-            .map(x -> x * x)
-            .collect(Collectors.toList());
-
-        //25. Find and print the distinct odd numbers
-        numberList1
-            .stream()
-            .filter(x -> x % 2 != 0)
-            .collect(Collectors.toList());
-
-        //26. Find the union of two lists of integers
-        List<Integer> combinedNumberList = Stream
-            .concat(numberList1.stream(), numberList2.stream())
-            .collect(Collectors.toList());
-
-        //27 Find the kth smallest element in a list of integers
-        Integer thirdSmallestInteger = numberList1
-            .stream()
-            .sorted()
-            .skip(2)
-            .findFirst()
-            .get();
-
-        //28. Remove all non-numeric characters from a list
-        List<Integer> extractedIntegers = alphanumericList
-            .stream()
-            .map(s -> {
-                String numericString = Arrays
-                    .stream(s.split(""))
-                    .filter(c -> {
-                        try {
-                            Integer.parseInt(c);
-                            return true;
-                        } catch (Exception e) {
-                            return false;
+        String[] anagramQuestion = {"pat", "tap", "pan", "nap", "team", "tree", "meat"};
+        List<List<String>> groupedAnagrams = Arrays
+                .stream(anagramQuestion)
+                .collect(Collectors.groupingBy(
+                        s -> {
+                            char[] chars = s.toCharArray();
+                            Arrays.sort(chars);
+                            return new String(chars);
                         }
-                    })
-                    .collect(Collectors.joining());
-                return Integer.parseInt(numericString);
-            })
-            .collect(Collectors.toList());
-
-        // extractedIntegers = alphanumericList
-        //     .stream()
-        //     .map(s -> Integer.parseInt(s.replace("^[0-9]+", "")))
-        //     .collect(Collectors.toList());
-
-        //29. Find and print strings containing only digits
-        List<String> nonNumericStrings = Arrays
-            .stream(mixedList)
-            .filter(s -> s.replaceAll("^[0-9]+", "").length() == s.length())
-            .collect(Collectors.toList());
-        
-        //30. Convert a list of strings to uppercase
-        List<String> uppercaseNameList = nameList
-            .stream()
-            .map(s -> s.toUpperCase())
-            .collect(Collectors.toList());
-
-        //31. Calculate the average of all the numbers
-        numberList1
-            .stream()
-            .mapToInt(x -> x)
-            .average()
-            .getAsDouble();
-
-        //32. Find the intersection of two lists using Java streams
-        List<Integer> intersectionList = numberList1
-            .stream()
-            .filter(x -> numberList2.contains(x))
-            .collect(Collectors.toList());
-
-        //33. Find the occurrence of domains using Java streams
-        Map<String, Long> emailDomainCount = employees
-            .stream()
-            .map(employee -> employee.getEmail().get(0).substring(employee.getEmail().get(0).indexOf("@")))
-            .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
-
-        //34. Generate the first 10 numbers of the Fibonacci Sequence
-        List<Integer> fibonacciSequence = Stream
-            .iterate(new int[] {1, 1}, arr -> new int[] {arr[1], arr[0] + arr[1]})
-            .limit(10)
-            .map(arr -> arr[0])
-            .collect(Collectors.toList());
-
-        //35. Convert list of integers to a list of their squares
-        List<Integer> squaredNumberList = numberList1
-            .stream()
-            .map(x -> x * x)
-            .collect(Collectors.toList());
-
-        //36. Transform Person object stream into a single string
-        String employeeNamesJoined = employees
-            .stream()
-            .map(o -> o.getName().toUpperCase())
-            .collect(Collectors.joining("|"));
-
-        //37 Group list of strings by their first character and count the number of strings
-        Map<Character, List<String>> groupedNamesByFirstCharacter = nameList
-            .stream()
-            .collect(Collectors.groupingBy(x -> x.charAt(0)));
-            
-        //38. Convert a list to a map
-        numberList1
-            .stream()
-            .map(x-> {
-                System.out.println("TEST:3");
-                return x;
-            });
-        List<Integer> test = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        test
-            .stream()
-            .filter(x -> {
-                System.out.println("TEST:0");
-                return x % 5 == 0;
-            })
-            .map(x -> {
-                System.out.println("TEST: 1");
-                return x; // Ensure the lambda returns a value
-            })
-            .findFirst();
-        //39 Multiply array elements
-        //40. Can we reuse a stream in Java?
-        //41 Convert a list of string to uppercase and then concatenate
-        //41. Difference between map and flatMap
-        //43. Concatenate 2 streams
-        //44. Given a person list, fetch the list of name
-        // -whose age is greater than 30
-        // -name should be unique
-        // -names should be in sorted order
-        //45. Print distinct numbers which starts with "1" in descending order
-        //46. Return comparison of a Person object based on first name and then last name
+                ))
+                .entrySet()
+                .stream()
+                .map(e -> e.getValue())
+                .toList();
     }
 }
